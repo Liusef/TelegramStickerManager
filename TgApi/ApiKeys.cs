59a1fcacb4e6e2853b3ea6e9692f69b7682c0c93
Liusef <liujoseph2920@Outlook.com>
2022-01-03 01:28:54 -0800

    Delete Apikeys file

--- TgApi/ApiKeys.cs
+++
@@ -1,7 +0,0 @@
-namespace TgApi;
-
-public class ApiKeys
-{
-    public const int ApiId = 0; // API ID goes here, replace the 0 
-    public const string ApiHash = ""; // API Hash string goes here (inside the quotation marks)
-}
