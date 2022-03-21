15e9c28a24c9a2e0d74e3a6774d87496dfd9762b
Liusef <32256890+Liusef@users.noreply.github.com>
2022-03-21 01:48:48 -0400

    Implemented Replacing Stickers

--- ReunionApp/Pages/CommandPages/ReplaceSticker.xaml.cs
+++ ReunionApp/Pages/CommandPages/ReplaceSticker.xaml.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
+using ReunionApp.Runners;
+using TgApi.Types;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -22,8 +29,89 @@ namespace ReunionApp.Pages.CommandPages;
 /// </summary>
 public sealed partial class ReplaceSticker : Page
 {
-    public ReplaceSticker()
+    private ObservableCollection<ReplaceStickerUpdate> stickers = new ObservableCollection<ReplaceStickerUpdate>();
+
+    public ReplaceSticker() => this.InitializeComponent();
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
     {
-        this.InitializeComponent();
+        base.OnNavigatedTo(e);
+        var p = e.Parameter as Sticker[];
+        foreach (var s in p) stickers.Add(new ReplaceStickerUpdate { Sticker = s });
+    }
+
+    private void Back(object sender, RoutedEventArgs e) => Frame.GoBack();
+
+    private async Task<bool> FindErrors()
+    {
+        return false;
+    }
+
+    private async void Finish(object sender, RoutedEventArgs e)
+    {
+        if (await FindErrors()) return;
+
+        processing.Visibility = Visibility.Visible;
+        await Task.Run(async () => await StickerLogic.ResizeAllToStickerParallelAsync(stickers.ToArray()));
+        var runner = new ReplaceStickerRunner(stickers.ToArray());
+        Frame.Navigate(typeof(ProcessingCommand), runner, new DrillInNavigationTransitionInfo());
+    }
+
+    private async Task SetNewImg(ReplaceStickerUpdate update)
+    {
+        var file = await AppUtils.PickSingleFileAsync(AppUtils.ImageSharpFormats);
+        update.NewPath = file.Path;
+    }
+
+    private async Task ClearImg(ReplaceStickerUpdate update)
+    {
+        update.NewPath = " ";
+    }
+
+    private async void MenuFlyout_SetNewImg(object sender, RoutedEventArgs e)
+    {
+        var button = (MenuFlyoutItem) sender;
+        await SetNewImg(button.DataContext as ReplaceStickerUpdate);
+    }
+
+    private async void MenuFlyout_ClearImg(object sender, RoutedEventArgs e)
+    {
+        var button = (MenuFlyoutItem) sender;
+        await ClearImg(button.DataContext as ReplaceStickerUpdate);
+    }
+
+    private async void SplitButton_SetNewImg(SplitButton sender, SplitButtonClickEventArgs args) =>
+        await SetNewImg(sender.DataContext as ReplaceStickerUpdate);
+    
+}
+
+public class ReplaceStickerUpdate : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private string _newPath = " ";
+
+
+    public Sticker Sticker { get; set; }
+    public string NewPath
+    {
+        get => _newPath;
+        set
+        {
+            _newPath = value;
+            OnPropertyChanged();
+            OnPropertyChanged("OldVisibility");
+        }
+    }
+    public string ThreadsafeNewPath
+    {
+        get => _newPath;
+        set => _newPath = value;
+    }
+    public Visibility OldVisibility => string.IsNullOrWhiteSpace(NewPath) ? Visibility.Visible : Visibility.Collapsed;
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
---
+++ ReunionApp/Runners/ReplaceStickerRunner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReunionApp.Pages.CommandPages;
+using TgApi.Telegram;
+using static TdLib.TdApi.InputFile;
+
+namespace ReunionApp.Runners;
+public class ReplaceStickerRunner : CommandRunner
+{
+    private ReplaceStickerUpdate[] updates;
+    private int _index;
+    private int Index
+    {
+        get => _index;
+        set
+        {
+            _index = value;
+            Progress = 100 * (_index + 0.0) / updates.Length;
+        }
+    }
+
+    public ReplaceStickerRunner(ReplaceStickerUpdate[] updates) => this.updates = updates;
+
+    public async override Task RunCommandsAsync()
+    {
+        var client = App.GetInstance().Client;
+        var botId = await client.GetIdFromUsernameAsync("Stickers");
+        var waiter = new MessageWaiter(client, botId);
+
+        await waiter.SendMsgAndAwaitNext("/cancel");
+        await SendAndAddToOutputsAsync(waiter, "/replacesticker");
+
+        for (Index = 0; Index < updates.Length; Index++)
+        {
+            if (!File.Exists(updates[Index].NewPath)) continue;
+
+            var r = await SendDocumentAndAddToOutputsAsync(waiter, new InputFileRemote { Id = updates[Index].Sticker.RemoteFileId },
+                new CommandOutput(null, updates[Index].Sticker.BestPath, true));
+
+            if (r[..7] != "Alright") continue;
+
+            r = await SendImageAndAddToOutputsAsync(waiter, updates[Index].NewPath);
+
+            if (r[..10] != "I replaced")
+            {
+                await SendAndAddToOutputsAsync(waiter, "/cancel");
+                await SendAndAddToOutputsAsync(waiter, "/replacesticker");
+            }
+        }
+        await SendAndAddToOutputsAsync(waiter, "/done");
+        await SendAndAddToOutputsAsync(waiter, "/cancel");
+    }
+}
